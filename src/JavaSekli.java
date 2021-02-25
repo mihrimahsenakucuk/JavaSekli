9bfaed3c32774f8af8e1b25b3863172876e2eccb
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-02-25 22:34:04 +0300

    Update JavaSekli.java

--- src/JavaSekli.java
+++ src/JavaSekli.java
@@ -2,10 +2,10 @@ public class JavaSekli {
 
     public static void main(String[] args) {
 
-        System.out.println("   J    a   v     v  a ");
-        System.out.println("   J   a a   v   v  a a");
-        System.out.println("J  J  aaaaa   V V  aaaaa");
-        System.out.println(" JJ  a     a   V  a     a");
+        System.out.println("   J       a      v     v     a ");
+        System.out.println("   J      a a      v   v     aaa");
+        System.out.println("J  J     aaaaa      V V     aaaaa");
+        System.out.println(" JJ     a     a      V     a     a");
     }
 
 }
